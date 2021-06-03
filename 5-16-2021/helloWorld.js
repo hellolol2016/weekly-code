622829c15663857735b3d9a0586d138d51e2e262
hellolol2016 <dennisthederp812@gmail.com>
2021-06-02 19:17:29 -0700

    new button for js

--- 5-16-2021/helloWorld.js
+++ 5-16-2021/helloWorld.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const btn = document.querySelector('#btn');
+btn.addEventListener('click',() =>{
+    alert("Hello World");
+});
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click',() =>{
+    console.log("Hello World");
+});
