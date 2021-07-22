6353fe32ff59d14d76a840c66018530dc3189f11
hellolol2016 <dennisthederp812@gmail.com>
2021-07-21 21:52:20 -0700

    extension week 1

---
+++ 7-18-2021 google extension/forms n stuf/form.js
@@ -0,0 +1,8 @@
+const submit = document.querySelector("#submit")
+
+submit.addEventListener("click",function(){
+    let email = document.querySelector("#email").value
+    let name = document.querySelector("#username").value;
+    
+    
+})
